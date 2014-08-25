0f84dd85a4bebb77a28af96a708c523343965acd
RobinROAR <zrb915@live.com>
2014-08-25 09:31:58 +0800

    fix bu  Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -4,26 +4,31 @@ from numpy import *
 
 st = time.time();
 
-image = Image.open('image/star.jpg') 
+image = Image.open('image/1.jpg') 
 #转换称灰度图，转成矩阵
 mat = array(image.convert("L"),'f')	
-s = rr.regiongrowing(mat,5,16,(80,80))
+
+s = rr.regiongrowing(mat,20,24,(160,190))
 #写入模块	
 image.load()
 putpixel = image.im.putpixel   #fill pixel
 
-for j in range(image.size[1]):
-	for i in range(image.size[0]):
+#矩阵和图像的坐标位相反
+
+for i in range(image.size[1]):
+	for j in range(image.size[0]):
 		if (i,j) not in s:
-			putpixel( (i,j),20)
+			putpixel( (j,i),20)
 		else:
-			putpixel( (i,j),150)
+			putpixel( (j,i),150)
 			
 	
 output = "image/new"
 image.thumbnail( (image.size[0], image.size[1] ), Image.ANTIALIAS )
 image.save(output + ".JPEG","JPEG")
+
 print image.size[0]*image.size[1]
+print image.size[0],image.size[1]
 	
 et = time.time();
 print 'It took '+ str(et-st)+'  s'
